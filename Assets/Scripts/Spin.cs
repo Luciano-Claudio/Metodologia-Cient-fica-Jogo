dc4bbead8ca69d266e148c18bbd62b9d9fe66a85
Luciano Claudio <lucianoclaudio.se@gmail.com>
2023-04-11 16:04:45 -0300

    Criação da base do jogo

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PickerWheelUI;
+using UnityEngine.UI;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] private Button uiSpinButton;
+    [SerializeField] private PickerWheel pickerWheel;
+
+    // Update is called once per frame
+    void Start()
+    {
+        uiSpinButton.onClick.AddListener(() =>
+        {
+            pickerWheel.OnSpinStart(() =>
+            {
+                Debug.Log("Spin Started..");
+            });
+            pickerWheel.OnSpinEnd(wheelPiece =>
+            {
+                Debug.Log("Spin end: Label:" + wheelPiece.Label + ", Amount:" + wheelPiece.Amount);
+            });
+            pickerWheel.Spin();
+        });
+    }
+}
